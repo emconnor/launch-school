f0e61dcff40fc948caf2aa1f867532fcc90b81ee
emconnor <emconnor@seas.upenn.edu>
2021-09-22 16:58:23 -0400

    updated displayWinner function

--- js_101_lesson2/rock_paper_scissors.js
+++ js_101_lesson2/rock_paper_scissors.js
@@ -4,21 +4,50 @@
 
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+function displayWinner(choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
-
-function prompt(message) {
-  console.log(`=> ${message}`);
+  if ((choice === 'rock' && computerChoice === 'scissors') ||
+    (choice === 'paper' && computerChoice === 'rock') ||
+    (choice === 'scissors' && computerChoice === 'paper')) {
+    prompt('You win!');
+  } else if ((choice === 'rock' && computerChoice === 'paper') ||
+    (choice === 'paper' && computerChoice === 'scissors') ||
+    (choice === 'scissors' && computerChoice === 'rock')) {
+    prompt('Computer wins!');
+  } else {
+    prompt("It's a tie!");
+  }
 }
 
-prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-let playerChoice = readline.question();
+while (true) {
 
-while (!VALID_CHOICES.includes(playerChoice)) {
-  prompt("Hmmm. That doesn't look like a valid choice. Try again!");
-  playerChoice = readline.question();
+  function prompt(message) {
+    console.log(`=> ${message}`);
+  }
+
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let playerChoice = readline.question();
+
+  while (!VALID_CHOICES.includes(playerChoice)) {
+    prompt("Hmmm. That doesn't look like a valid choice. Try again!");
+    playerChoice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(playerChoice, computerChoice);
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
+  }
+
+  if (answer[0] !== 'y') break;
+
 }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
 
-prompt(`You chose ${playerChoice}. The computer chose ${computerChoice}.`);
