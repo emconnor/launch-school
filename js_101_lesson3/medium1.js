e908bb9f54bef6259e0c107ddfe10286efafc024
emconnor <emconnor@seas.upenn.edu>
2021-09-23 12:31:37 -0400

    medium 1 initial commit

---
+++ js_101_lesson3/medium1.js
@@ -0,0 +1,47 @@
+/*
+Medium 1 Practice Problems
+*/
+
+/////// question 1 ///////
+// 
+
+/////// question 2 ///////
+/*
+The Array.prototype.reverse method reverses the order of elements in an array, and
+Array.prototype.sort can rearrange the elements in a variety of ways, including descending.
+Both of these methods mutate the original array as shown below. Write two distinct ways of
+reversing the array without mutating the original array. Use reverse for the first solution,
+and sort for the second.
+*/
+
+
+/////// question 3 ///////
+// 
+
+/////// question 4 ///////
+// 
+
+/////// question 5 ///////
+// 
+
+/////// question 6 ///////
+// 
+
+/////// question 7 ///////
+// 
+
+/////// question 8 ///////
+// 
+
+/////// question 9 ///////
+/*
+Back in the stone age (before CSS), we used spaces to align things on the screen. If we
+have a 40-character wide table of Flintstone family members, how can we center the
+following title above the table with spaces?
+*/
+
+
+/////// question 10 ///////
+/*
+Write two one-line expressions to count the number of lower-case t characters in each of the following strings:
+*/
