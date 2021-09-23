07a8bf7ffa5763fca8bbcb09ab83879d9103cc48
emconnor <emconnor@seas.upenn.edu>
2021-09-23 16:35:11 -0400

    hard1 initial commit

---
+++ js_101_lesson3/hard1.js
@@ -0,0 +1,19 @@
+/*
+Hard 1 Practice Problems
+*/
+
+/////// question 1 ///////
+// 
+
+/////// question 2 ///////
+// 
+
+/////// question 3 ///////
+// 
+
+/////// question 4 ///////
+/*
+
+*/
+
+
