42338b632213e8ca979dd99fd34071f59961182b
emconnor <emconnor@seas.upenn.edu>
2021-09-23 12:29:09 -0400

    easy 2 done

--- js_101_lesson3/easy2.js
+++ js_101_lesson3/easy2.js
@@ -111,4 +111,10 @@ console.log(title.padStart(padding + title.length));
 
 /////// question 10 ///////
 /*
+Write two one-line expressions to count the number of lower-case t characters in each of the following strings:
 */
+let statement1 = "The Flintstones Rock!";
+let statement2 = "Easy come, easy go.";
+
+statement1.split('').filter(char => char === 't').length;
+statement1.split('').filter(char => char === 't').length;
